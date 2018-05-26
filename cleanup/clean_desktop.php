b24b7ab945cb50d52bb7b5b41537c04930da4585
Reagan Wilkins <reagan.wilkins@gmail.com>
2018-05-26 13:52:07 -0500

    Tested out DRY solution

--- cleanup/clean_desktop.php
+++ cleanup/clean_desktop.php
@@ -3,29 +3,18 @@ require_once("../.local_paths.php");
 
 function removeFiles($extension)
 {
+	$files = false;
 	echo "Removing {$extension} files..." . PHP_EOL;
 	foreach(glob("{$desktop}/{$extension}") as $file) {
-
+		$files = true;
+		echo "Moving file: " . basename($file) . " off of Desktop..." . PHP_EOL;
+		rename($file, "{$desktop}/pictures/" . basename($file));
+	}
+	if(!$files) {
+		echo "No {$extension} files to remove..." . PHP_EOL;
 	}
 }
 // Remove .jpg files
-$jpgs = false;
-echo "Removing .jpg files" . PHP_EOL;
-foreach(glob("{$desktop}/*.jpg") as $file) {
-	$jpgs = true;
-	echo "Moving file: " . basename($file) . " off of Desktop..." . PHP_EOL;
-	rename($file, "{$desktop}/pictures/" . basename($file));
-}
-if($jpgs == false) {
-	echo "No .jpg files to remove..." . PHP_EOL;
-}
 
-// Remove .png files
-$pngs = false;
-echo "Removing .png files" . PHP_EOL;
-foreach(glob("{$desktop}/*.png") as $file)
-
-// Remove .pdf files
-
-// Remove Screenshots
+removeFiles("*.jpg");
 ?>
