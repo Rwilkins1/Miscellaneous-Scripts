225be493bdceb77184d011f74fc4afdbebdd8194
Reagan Wilkins <reagan.wilkins@gmail.com>
2018-05-26 13:48:21 -0500

    Started cleaning up the code and making it DRYer

--- cleanup/clean_desktop.php
+++ cleanup/clean_desktop.php
@@ -1,7 +1,16 @@
 <?php
 require_once("../.local_paths.php");
+
+function removeFiles($extension)
+{
+	echo "Removing {$extension} files..." . PHP_EOL;
+	foreach(glob("{$desktop}/{$extension}") as $file) {
+
+	}
+}
+// Remove .jpg files
 $jpgs = false;
-echo $desktop . PHP_EOL;
+echo "Removing .jpg files" . PHP_EOL;
 foreach(glob("{$desktop}/*.jpg") as $file) {
 	$jpgs = true;
 	echo "Moving file: " . basename($file) . " off of Desktop..." . PHP_EOL;
@@ -10,4 +19,13 @@ foreach(glob("{$desktop}/*.jpg") as $file) {
 if($jpgs == false) {
 	echo "No .jpg files to remove..." . PHP_EOL;
 }
+
+// Remove .png files
+$pngs = false;
+echo "Removing .png files" . PHP_EOL;
+foreach(glob("{$desktop}/*.png") as $file)
+
+// Remove .pdf files
+
+// Remove Screenshots
 ?>
