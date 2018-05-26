16682ccd878bc67b58d27fbd08574e4fbc4c9c8b
Reagan Wilkins <reagan.wilkins@gmail.com>
2018-05-26 13:43:29 -0500

    Tested out .jpg logic

--- cleanup/clean_desktop.php
+++ cleanup/clean_desktop.php
@@ -4,6 +4,10 @@ $jpgs = false;
 echo $desktop . PHP_EOL;
 foreach(glob("{$desktop}/*.jpg") as $file) {
 	$jpgs = true;
-	echo "Moving file: " . $file . " off of Desktop..." . PHP_EOL;
+	echo "Moving file: " . basename($file) . " off of Desktop..." . PHP_EOL;
+	rename($file, "{$desktop}/pictures/" . basename($file));
+}
+if($jpgs == false) {
+	echo "No .jpg files to remove..." . PHP_EOL;
 }
 ?>
