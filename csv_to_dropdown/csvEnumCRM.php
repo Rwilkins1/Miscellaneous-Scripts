eef07474e1d9f7349b42fb5020c5be06d03438d9
Reagan Wilkins <reagan.wilkins@gmail.com>
2018-05-25 19:40:03 -0500

    Added post-installation logic to the CRM version

--- csv_to_dropdown/csvEnumCRM.php
+++ csv_to_dropdown/csvEnumCRM.php
@@ -10,45 +10,87 @@ or Suite CRM environment.
 
 Run Quick Repair and Rebuild after execution.
 */
+if(filesize('directoryConfig.php') == 0) {
+  die("
+  The installer has not been run. Please enter the following command:
+  'php install.php'
+  and follow the prompts to complete the installation process.
+  " . PHP_EOL);
+}
+
+function getInput()
+{
+  $handle = fopen("php://stdin", "r");
+  $input = trim(fgets($handle));
+  fclose($handle);
+  return $input;
+}
+
+function getLanguage($language, $defaultLanguage)
+{
+  $languagesArray = ["zh_TW", "zh_CN", "uk_UA", "tr_TR", "th_TH", "sv_SE",
+  "sr_RS", "sq_AL", "sk_SK", "ru_RU", "ro_RO", "pt_PT", "pt_BR", "pl_PL",
+  "nl_NL", "nb_NO", "lv_LV", "lt_LT", "ko_KR", "ja_JP", "it_it", "hu_HU",
+  "hr_HR", "he_IL", "fr_FR", "fi_FI", "et_EE", "es_LA", "es_ES", "en_us",
+  "en_UK", "el_EL", "de_DE", "da_DK", "cs_CZ", "ca_ES", "bg_BG", "ar_SA"];
+
+  if(array_search($language, $languagesArray) || $language == $languagesArray[0]) {
+    return $language;
+  } else if(strtolower($language) == 'cancel') {
+    return $defaultLanguage;
+  } else {
+    return false;
+  }
+}
 
 function setConfiguration($argv)
 {
-  ini_set('auto_detect_line_endings', true);
+  require_once('directoryConfig.php');
   if($argv[1] == "--help") {
     showHelpPage();
     return;
   }
   $overrideRoot = array_search("-or", $argv);
   if($overrideRoot) {
+    echo "Overriding Root Directory..." . PHP_EOL;
+    sleep(3);
+    if(substr($argv[$overrideRoot + 1], -1) != "/") {
+      $argv[$overrideRoot + 1] .= "/";
+    }
     define('ROOT_DIRECTORY', $argv[$overrideRoot + 1]);
   } else {
-    define('ROOT_DIRECTORY', '/Users/rwilkins/Documents/');
+    define('ROOT_DIRECTORY', $defaultRoot);
   }
 
   $overrideUpload = array_search("-ou", $argv);
   if($overrideUpload)  {
     echo "Overriding Upload Directory..." . PHP_EOL;
-    sleep(2);
+    sleep(3);
+    if(substr($argv[$overrideUpload + 1], -1) != "/") {
+      $argv[$overrideUpload + 1] .= "/";
+    }
     define('UPLOAD_DIRECTORY', $argv[$overrideUpload + 1]);
   } else {
-    define('UPLOAD_DIRECTORY', '/Users/rwilkins/Documents/');
+    define('UPLOAD_DIRECTORY', $defaultUpload);
   }
 
   $overrideLanguage = array_search("-l", $argv);
   if($overrideLanguage) {
-    $languagesArray = ["zh_TW", "zh_CN", "uk_UA", "tr_TR", "th_TH", "sv_SE",
-    "sr_RS", "sq_AL", "sk_SK", "ru_RU", "ro_RO", "pt_PT", "pt_BR", "pl_PL",
-    "nl_NL", "nb_NO", "lv_LV", "lt_LT", "ko_KR", "ja_JP", "it_it", "hu_HU",
-    "hr_HR", "he_IL", "fr_FR", "fi_FI", "et_EE", "es_LA", "es_ES", "en_us",
-    "en_UK", "el_EL", "de_DE", "da_DK", "cs_CZ", "ca_ES", "bg_BG", "ar_SA"];
-    $input = $argv[$overrideLanguage + 1];
-    if(array_search($input, $languagesArray) || $input == $languagesArray[0]) {
-      define('LANGUAGE', $input);
+    $language = getLanguage($argv[$overrideLanguage + 1], $defaultLanguage);
+    while($language === false) {
+      echo "The language key you entered is invalid, please enter a valid language key or type 'cancel' to default to American English" . PHP_EOL;
+      $input = getInput();
+      $language = getLanguage($input, $defaultLanguage);
+    }
+    if($language != $defaultLanguage) {
+      echo "Overriding Language..." . PHP_EOL;
+      sleep(3);
+      define('LANGUAGE', $language);
     } else {
-      die("Sorry, the specified language key is invalid. Please enter a valid language key" . PHP_EOL);
+      define('LANGUAGE', $defaultLanguage);
     }
   } else {
-    define('LANGUAGE', 'en_us');
+    define('LANGUAGE', $defaultLanguage);
   }
   openCsv();
 }
@@ -56,8 +98,7 @@ function setConfiguration($argv)
 function openCsv()
 {
   echo "Enter the name of the CSV file" . PHP_EOL;
-  $handle = fopen("php://stdin", "r");
-  $file = preg_replace('/[^A-ZA-z0-9_.\-]/', '', fgets($handle));
+  $file = getInput();
   $fh = fopen(UPLOAD_DIRECTORY . $file, 'r');
   // echo $fh . PHP_EOL;
   if($fh === false) {
@@ -67,14 +108,10 @@ function openCsv()
   $header = fgetcsv($fh);
 
   echo "Enter the column name to turn into a dropdown" . PHP_EOL;
-  $handle = fopen("php://stdin", "r");
-  $column = preg_replace('/[^A-ZA-z0-9_.\-]/', '', fgets($handle));
-  fclose($handle);
+  $column = getInput();
 
   echo "Enter the name of the dropdown list to create" . PHP_EOL;
-  $handle = fopen("php://stdin", "r");
-  $list = preg_replace('/[^A-ZA-z0-9_.\-]/', '', fgets($handle));
-  fclose($handle);
+  $list = getInput();
 
   $code = '<?php
 
@@ -102,6 +139,7 @@ function openCsv()
   fclose($fh);
   $code .= "'' => '',
 );";
+
   createDropdown($list, $code);
 }
 
@@ -116,7 +154,6 @@ function cleanDropdownItem($item)
 function createDropdown($list, $code)
 {
   $fh = fopen(ROOT_DIRECTORY . "Extension/application/Ext/Language/" . LANGUAGE . ".{$list}.php", 'w');
-
   if($fh === false) {
     die("File Handle is false. Please check filepath for root directory: " . ROOT_DIRECTORY . PHP_EOL);
   }
