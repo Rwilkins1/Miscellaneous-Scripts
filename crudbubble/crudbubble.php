a0db3b40ac1075e3d430d9b42ac20949b4d37e04
Reagan Wilkins <reagan.wilkins@gmail.com>
2020-02-10 17:17:58 -0600

    Initial crudbubble commit

---
+++ crudbubble/crudbubble.php
@@ -0,0 +1,22 @@
+<?php
+
+// prompts the script's user for an answer
+function readInput($prompt)
+{
+
+}
+
+// If a directory is needed, builds it, if not, adds files to existing one
+function buildAddDirectory($action, $name)
+{
+
+}
+
+// creates the file in question
+function buildFile($file)
+{
+
+}
+
+
+?>
