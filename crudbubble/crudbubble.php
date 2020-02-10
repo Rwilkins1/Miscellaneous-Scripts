4ed7cf50c57443b03cc214282dd176d1ec354766
Reagan Wilkins <reagan.wilkins@gmail.com>
2020-02-10 17:30:12 -0600

    Added author information and description

--- crudbubble/crudbubble.php
+++ crudbubble/crudbubble.php
@@ -1,4 +1,13 @@
 <?php
+/*
+Author: Reagan Wilkins
+reagan.wilkins@gmail.com
+github.com/Rwilkins1
+02/10/2020
+
+Creates a bare bones CRUD application, 
+eliminating the need to manually create a file for each step.
+*/
 
 // prompts the script's user for an answer
 function readInput($prompt)
